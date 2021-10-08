d1a6a505c66b12ee6d69f454b54fbbc676675169
ShantanuWaikar10 <89289274+ShantanuWaikar10@users.noreply.github.com>
2021-10-08 16:38:50 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "5fba667e2abc30105a31cbb0abdfb691"
+    }
+}
+
+module.exports = constants;
